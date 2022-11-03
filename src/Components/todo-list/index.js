6b97942a182ab4184345acad31b951f328833e4c
Robert Shepley <shepleysound@gmail.com>
2022-11-03 15:36:58 -0700

    Replace hand-rolled animation with framer motion.

--- src/Components/todo-list/index.js
+++ src/Components/todo-list/index.js
@@ -1,13 +1,7 @@
-import { useEffect, useState, useContext, useRef } from 'react';
-import { Stack, Pagination, Paper } from '@mantine/core';
+import { useEffect, useState, useContext } from 'react';
+import { Stack, Pagination } from '@mantine/core';
 import { SettingsContext } from '../../Context/settings';
-import {
-  CSSTransition,
-  TransitionGroup,
-  SwitchTransition,
-} from 'react-transition-group';
-import 'animate.css';
-import './style.css';
+import { motion, AnimatePresence } from 'framer-motion';
 import Todo from '../todo';
 
 export default function TodoList({
@@ -39,51 +33,26 @@ export default function TodoList({
   return (
     <>
       <Stack>
-        <SwitchTransition>
-          <CSSTransition
-            key={activePage}
-            timeout={500}
-            classNames={{
-              appear: 'animate__animated',
-              appearActive: 'animate__animated animate__fadeInRight',
-              enter: 'opacity_0',
-              enterActive: 'opacity_transition opacity_100',
-              exit: 'animate__animated',
-              exitActive: 'animate_animated animate__fadeOutRight opacity_0',
-              exitDone: 'opacity_0',
-            }}
-          >
-            <Stack>
-              <TransitionGroup component={null}>
-                {displayList.map((item, i) => (
-                  <CSSTransition
-                    key={item.id}
-                    nodeRef={item.nodeRef}
-                    classNames={{
-                      // appear: 'animate__animated',
-                      appearActive: 'animate__animated animate__fadeInRight',
-                      // enter: 'animate__animated',
-                      enterActive: 'animate__animated animate__fadeIn',
-                      // exit: 'animate__animated',
-                      exitActive: 'animate__animated animate__backOutUp',
-                    }}
-                    timeout={{
-                      enter: 500,
-                      exit: 500,
-                    }}
-                  >
-                    <Todo
-                      key={item.id}
-                      item={item}
-                      toggleComplete={toggleComplete}
-                      deleteItem={deleteItem}
-                    />
-                  </CSSTransition>
-                ))}
-              </TransitionGroup>
-            </Stack>
-          </CSSTransition>
-        </SwitchTransition>
+        <Stack>
+          <AnimatePresence mode='popLayout'>
+            {displayList.map((item) => (
+              <motion.div
+                layout
+                key={item.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <Todo
+                  key={item.id}
+                  item={item}
+                  toggleComplete={toggleComplete}
+                  deleteItem={deleteItem}
+                />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </Stack>
         <Pagination
           page={activePage}
           onChange={(val) => {
